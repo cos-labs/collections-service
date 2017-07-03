1064cb33d0aa1bbda079125677c581ea98cbb0ea
Josh Bird <joshua.thomas.bird@gmail.com>
2017-07-03 14:23:47 -0400

    Set up database url for heroku

--- service/settings/production.py
+++ service/settings/production.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+import dj_database_url
 
 from .base import *
 
@@ -9,7 +10,7 @@ DEBUG=False
 SECRET_KEY = os.environ['SECRET_KEY']
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = 'https://cos-labs.github.io/collections-service/'
-
+DATABASES['default'] = dj_database_url.config()
 
 try:
     from .local import *
