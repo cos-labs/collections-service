19b470d38f0426e55663500391ffd7a040482c7d
Cameron Blandford <cam@cos.io>
2017-08-10 13:46:58 -0400

    Remove unnecessary comments, make tests more specific

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -4,38 +4,30 @@ from django.urls import reverse
 
 class CollectionTest(TestBase):
     def test_logged_in_user_can_post_collection(self):
-        # log in as owner
         self.client.login(username="owner", password="password123")
         # try to post a collection
         response = self.client.post(reverse('collection-list'),
                                     {'title': 't', 'description': 'd', 'source': 'mst3k', 'tags': ''})
-        # assert success
         self.assertEqual(response.status_code, 201)
 
     def test_user_can_edit_own_collection(self):
-        # log in as owner
         self.client.login(username="owner", password="password123")
         # attempt to patch collection
         response = self.client.patch(reverse('collection-detail', args=[self.collection.id]),
                                      {'title': 'New Title'})
-        # assert success
         self.assertEqual(response.status_code, 200)
 
     def test_user_cannot_edit_others_collections(self):
-        # log in as random user
         self.client.login(username="random", password="password123")
         # attempt to patch owner's collection
         response = self.client.patch(reverse('collection-detail', args=[self.collection.id]),
                                      {'title': 'New Title 2'})
-        # assert failure
-        self.assertNotEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 403)
 
     def test_user_can_delete_own_collection(self):
-        # log in as collection's owner
         self.client.login(username="owner", password="password123")
-        # attempt to delete owner's collection
+        # attempt to delete own collection
         response = self.client.delete(reverse('collection-detail', args=[self.collection.id]))
-        # assert success
         self.assertEqual(response.status_code, 204)
 
     def test_logged_out_cannot_post_collection(self):
@@ -47,8 +39,9 @@ class CollectionTest(TestBase):
     def test_cannot_delete_other_users_collection(self):
         self.client.login(username="random", password="password123")
         response = self.client.delete(reverse('collection-detail', args=[self.collection.id]))
-        self.assertNotEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, 403)
 
+    def test_logged_out_user_cannot_delete_collection(self):
         self.client.logout()
         response = self.client.delete(reverse('collection-detail', args=[self.collection.id]))
         self.assertNotEqual(response.status_code, 204)
--- tests/test_item.py
+++ tests/test_item.py
@@ -4,6 +4,7 @@ from . import factories
 from guardian.shortcuts import assign_perm
 from api import models
 
+
 class ItemTest(TestBase):
 
     def test_submitter_can_edit_own_item(self):
@@ -16,33 +17,29 @@ class ItemTest(TestBase):
         self.client.login(username='rando', password='password123')
         response = self.client.patch(reverse('collection-item-detail', args=[self.collection.id, self.item.id]),
                                     {'title': 'New Title'})
-        self.assertNotEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 401)
 
     def test_can_post_item_to_collection(self):
-        # log in as owner
         self.client.login(username="owner", password="password123")
         # try to post a collection
         response = self.client.post(reverse('collection-item-list', args=[self.collection.id]),
                                     {'title': 't', 'description': 'd', 'source': 'mst3k', 'tags': '',
                                      'category': 'none', 'status': 'pending', 'type': 'preprint'})
-        # assert success
         self.assertEqual(response.status_code, 201)
 
     def test_cannot_post_item_without_being_logged_in(self):
-        # log in as owner
         self.client.logout()
         # try to post a collection
         response = self.client.post(reverse('collection-item-list', args=[self.collection.id]),
                                     {'title': 't', 'description': 'd', 'source': 'mst3k', 'tags': '',
                                      'category': 'none', 'status': 'pending', 'type': 'preprint'})
-        # assert success
         self.assertEqual(response.status_code, 401)
-    #
+
     def test_non_collection_owner_cannot_change_item_status(self):
         self.client.login(username="rando", password="password123")
         response = self.client.patch(reverse('collection-item-detail', args=[self.collection.id, self.item.id]),
                                     {'status': 'rejected'})
-        self.assertNotEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 401)
 
     # item status can be changed by people with the "approve items" permission or owners of collections
     def collection_owner_can_change_item_status(self):
@@ -58,21 +55,17 @@ class ItemTest(TestBase):
         self.client.login(username="jane", password="password123")
         response = self.client.patch(reverse('collection-item-detail', args=[self.collection.id, self.item.id]),
                                      {'status': 'approved'})
-        import pprint; pprint.pprint(response.__dict__)
         self.assertEqual(response.status_code, 200)
 
     # item status is automatically set to approved if the collection has the "allow_all" setting set to true
     def test_allow_all_setting_auto_accepts_new_items(self):
         self.client.login(username="submitter", password="password123")
         c = factories.CollectionFactory(created_by=self.owner, settings={"allow_all": "True", "collection_type": "meeting"})
-        # make new collection
         c.save()
         response = self.client.post(reverse('collection-item-list', args=[c.id]),
                                     {'title': 't', 'description': 'd', 'source': 'mst3k', 'tags': '',
                                      'category': 'none', 'status': 'pending', 'type': 'preprint'})
-        item_id = int(response.__dict__['data']['id'])
-        i = models.Item.objects.get(pk=item_id)
-        self.assertEqual(i.status, "approved")
+        self.assertEqual(response.data['status'], "approved")
 
     # owner of a collection can delete items not theirs from collection
     def collection_owners_can_delete_any_item_from_their_collection(self):
@@ -85,7 +78,7 @@ class ItemTest(TestBase):
     def test_cannot_delete_others_item(self):
         self.client.login(username="rando", password="password123")
         response = self.client.delete(reverse('collection-item-detail', args=[self.collection.id, self.item.id]))
-        self.assertNotEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, 401)
 
     def test_can_delete_own_item(self):
         self.client.login(username="submitter", password="password123")
