4aca8d7911e906d64403b2cb6dcb2defc96a7ece
Cameron Blandford <cam@cos.io>
2017-08-16 16:04:45 -0400

    Increase page size of collection.items endpoint

--- service/settings/base.py
+++ service/settings/base.py
@@ -123,7 +123,7 @@ USE_TZ = True
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework_json_api.pagination.PageNumberPagination',
