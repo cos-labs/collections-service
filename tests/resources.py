b2c882e170ca33f0bcd0b34e66f121b8de410d9d
Cameron Blandford <cam@cos.io>
2017-10-30 12:19:38 -0400

    Update component names to match frontend changes

--- tests/resources.py
+++ tests/resources.py
@@ -10,42 +10,42 @@ meeting_json = {
     "layers": [
         {
             "sectionHeader": "Landing Title",
-            "component": "landing-title",
+            "component": "section-title",
             "settings": {}
         },
         {
             "sectionHeader": "Paragraph",
-            "component": "landing-paragraph",
+            "component": "section-paragraph",
             "settings": {
                 "body": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
             }
         },
         {
             "sectionHeader": "File Grid",
-            "component": "dataset-file-grid",
+            "component": "section-file-grid",
             "settings": {}
         },
         {
             "sectionHeader": "Splash Image",
-            "component": "landing-splash-image",
+            "component": "section-splash-image",
             "settings": {}
 
         },
         {
             "sectionHeader": "Splash Image",
-            "component": "contributor-list",
+            "component": "section-contributors",
             "settings": {
                 "background-color": "#fff"
             }
         },
         {
             "sectionHeader": "Schedule",
-            "component": "meeting-schedule",
+            "component": "section-schedule",
             "settings": {}
         },
         {
             "sectionHeader": "Items",
-            "component": "item-table",
+            "component": "section-item-table",
             "settings": {
                 "header-color": "#444",
                 "text-color": "#fff"
@@ -53,7 +53,7 @@ meeting_json = {
         },
         {
             "sectionHeader": "Sponsors",
-            "component": "landing-sponsors",
+            "component": "section-sponsors",
             "settings": {
                 "title": "Sponsors",
                 "data": "sponsor-categories",
@@ -63,7 +63,7 @@ meeting_json = {
         },
         {
             "sectionHeader": "Code of Conduct",
-            "component": "landing-paragraph",
+            "component": "section-paragraph",
             "settings": {
                 "title": "Code of Conduct",
                 "body": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
@@ -193,130 +193,24 @@ dataset_json = {
     },
     "layout": [
         {
-            "type": "landing-title",
+            "type": "section-title",
         },
         {
-            "type": "landing-paragraph",
+            "type": "section-paragraph",
             "body": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
         },
         {
-            "type": "landing-splash-image"
+            "type": "section-splash-image"
         },
         {
-            "type": "contributor-list",
+            "type": "section-contributors",
             "data": "speakers",
             "background-color": "#fff"
         },
         {
-            "type": "item-table",
+            "type": "section-item-table",
             "header-color": "#444",
             "text-color": "#fff"
         }
-    ],
-    "data": {
-        "presentations": [
-            {
-                "img-url": "http://placehold.it/32x32",
-                "presenter": "Rasmussen Bruce",
-                "title": "mollit tempor eiusmod pariatur aliqua eiusmod irure nostrud cupidatat nisi"
-            }
-        ],
-        "sponsorCategories": [
-            {
-                "category": "Gold",
-                "sponsors": [
-                    {
-                        "name": "Accenture",
-                        "website-url": "http://accenture.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/accenture-logo-199.png"
-                    },
-                    {
-                        "name": "Capital One",
-                        "website-url": "http://jobs.capitalone.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/capital-one-300.png"
-                    },
-                    {
-                        "name": "Google",
-                        "website-url": "http://google.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/google-new-302-300x99.png"
-                    },
-                    {
-                        "name": "Microsoft",
-                        "website-url": "http://microsoft.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/new-microsoft.jpg"
-                    }
-                ]
-            },
-            {
-                "category": "Silver",
-                "sponsors": [
-                    {
-                        "name": "Amazon",
-                        "website-url": "http://amazon.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/amazon-160.png "
-                    },
-                    {
-                        "name": "Qualcomm",
-                        "website-url": "http://qualcomm.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/qualcomm-logo-185.png"
-                    },
-                    {
-                        "name": "LinkedIn",
-                        "website-url": "http://linkedin.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/linkedin-160.png "
-                    },
-                    {
-                        "name": "Yahoo",
-                        "website-url": "http://yahoo.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/yahoo-168.png"
-                    }
-                ]
-            },
-            {
-                "category": "Bronze",
-                "sponsors": [
-                    {
-                        "name": "Twitter",
-                        "website-url": "http://twitter.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/twitter-88.png"
-                    },
-                    {
-                        "name": "PayPal",
-                        "website-url": "http://paypal.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/paypal-logo-with-name-210.png"
-                    },
-                    {
-                        "name": "Salesforce",
-                        "website-url": "http://salesforce.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/salesforce-96.png"
-                    },
-                    {
-                        "name": "Accenture",
-                        "website-url": "http://accenture.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/accenture-logo-199.png"
-                    },
-                    {
-                        "name": "Amazon",
-                        "website-url": "http://amazon.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/amazon-160.png "
-                    },
-                    {
-                        "name": "Qualcomm",
-                        "website-url": "http://qualcomm.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/qualcomm-logo-185.png"
-                    },
-                    {
-                        "name": "LinkedIn",
-                        "website-url": "http://linkedin.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/linkedin-160.png "
-                    },
-                    {
-                        "name": "Yahoo",
-                        "website-url": "http://yahoo.com",
-                        "img-url": "https://anitaborg.org/wp-content/uploads/2013/09/yahoo-168.png"
-                    }
-                ]
-            }
-        ]
-    }
+    ]
 }
