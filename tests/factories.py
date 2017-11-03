5c95f5334111893a1382168bddde9e3c58eea088
Ryan Mason <Rytiggy@gmail.com>
2017-11-03 13:16:37 -0400

    switch to point at prod not stage

--- tests/factories.py
+++ tests/factories.py
@@ -149,7 +149,7 @@ class ItemFactory(factory.Factory):
     title = factory.LazyFunction(science_sentence)
     description = factory.LazyFunction(science_description)
     status = 'approved'
-    source_id = 'xrfye'
+    source_id = '3hgm5'
 
 
 class CollectionFactory(factory.Factory):
